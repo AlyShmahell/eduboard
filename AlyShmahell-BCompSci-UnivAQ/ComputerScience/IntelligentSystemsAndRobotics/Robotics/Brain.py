4d6ab3f4ca372297d0f403c06443a6ed6c992c5b
AlyShmahell <aly.shmahell@gmail.com>
2016-11-13 21:03:25 +0100

    finished implementation

--- AlyShmahell-BCompSci-UnivAQ/ComputerScience/IntelligentSystemsAndRobotics/Robotics/Brain.py
+++ AlyShmahell-BCompSci-UnivAQ/ComputerScience/IntelligentSystemsAndRobotics/Robotics/Brain.py
@@ -1,9 +1,31 @@
 # Copyright 2016 Aly Shmahell
 
 # libraries
-
 from VrepAPIWrapper import *
 
+
+# initiation
 initFunction()
 
-mainLoop(1.0,1.0)
+# the brain of the robot: sense-calculate-act
+def brain():
+	backwardModeUntilTime=time.time()
+	while True:
+		state, force = senseFunction()
+		if state>0:
+			if abs(force[1])>1 or abs(force[2])>1:
+				backwardModeUntilTime=time.time()+2
+			if time.time()<backwardModeUntilTime:
+				actuateFunction(-2.0, 0)
+			else:
+				actuateFunction(8.0, 8.0)
+
+# the main function
+if __name__ == "__main__":
+	try:
+		brain()
+	except KeyboardInterrupt:
+		actuateFunction(0, 0)
+		
+
+
--- AlyShmahell-BCompSci-UnivAQ/ComputerScience/IntelligentSystemsAndRobotics/Robotics/VrepAPIWrapper.py
+++ AlyShmahell-BCompSci-UnivAQ/ComputerScience/IntelligentSystemsAndRobotics/Robotics/VrepAPIWrapper.py
@@ -2,6 +2,7 @@
 
 # libraries
 import sys
+import time
 sys.path.insert(0, './VrepPythonAPI')
 import vrep
 
@@ -17,21 +18,26 @@ def initFunction():
 	clientID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5) 
 	if clientID!=-1:
 		print ('Connected to remote API server')
+		actuateFunction(4.0, 4.0)
 	else:
 		sys.exit('Connection Failed!')
 
-# main loop
-def mainLoop(leftSpeed, rightSpeed):
-	while True:
-		# connect to motors
-		errorCode,leftHandle=vrep.simxGetObjectHandle(clientID,'dr12_leftJoint_',vrep.simx_opmode_oneshot_wait)
-		errorCode,rightHandle=vrep.simxGetObjectHandle(clientID,'dr12_rightJoint_',vrep.simx_opmode_oneshot_wait)
-		# initiate movement with a normal speed
-		errorCode=vrep.simxSetJointTargetVelocity(clientID, leftHandle, leftSpeed, vrep.simx_opmode_oneshot_wait)
-		errorCode=vrep.simxSetJointTargetVelocity(clientID, rightHandle, rightSpeed, vrep.simx_opmode_oneshot_wait)
-		# connect to sensor
-		errorCode,sensorHandle=vrep.simxGetObjectHandle(clientID,'dr12_bumperForceSensor_',vrep.simx_opmode_streaming)
-		return sensorHandle
+# actuation function: sends speed information to the motors
+def actuateFunction(leftSpeed, rightSpeed):
+	# connect to motors
+	errorCode,leftHandle=vrep.simxGetObjectHandle(clientID,'dr12_leftJoint_',vrep.simx_opmode_oneshot_wait)
+	errorCode,rightHandle=vrep.simxGetObjectHandle(clientID,'dr12_rightJoint_',vrep.simx_opmode_oneshot_wait)
+	# initiate movement with a normal speed
+	errorCode=vrep.simxSetJointTargetVelocity(clientID, leftHandle, leftSpeed, vrep.simx_opmode_oneshot_wait)
+	errorCode=vrep.simxSetJointTargetVelocity(clientID, rightHandle, rightSpeed, vrep.simx_opmode_oneshot_wait)
+
+# sensory function: returns collision information from the bumper
+def senseFunction():
+	# connect to sensor
+	errorCode,sensorHandle=vrep.simxGetObjectHandle(clientID,'dr12_bumperForceSensor_',vrep.simx_opmode_streaming)
+	returncode, state, force,torque=vrep.simxReadForceSensor(clientID, sensorHandle, vrep.simx_opmode_streaming)
+	return state, force
 
 # when the program ends, inform of a successful operation
-print('there are no problems!')
+if clientID==-1:
+	print('there are no problems!')
