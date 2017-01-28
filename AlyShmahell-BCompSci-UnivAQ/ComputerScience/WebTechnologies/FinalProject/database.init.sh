12c00e612b3e27e6ee63ef56571dfc00b074ad09
AlyShmahell <aly.shmahell@gmail.com>
2017-01-28 19:56:25 +0100

    second commit

--- AlyShmahell-BCompSci-UnivAQ/ComputerScience/WebTechnologies/FinalProject/database.init.sh
+++ AlyShmahell-BCompSci-UnivAQ/ComputerScience/WebTechnologies/FinalProject/database.init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo mysql -u root -p create database webtech;
-echo "create database webtech" | mysql -u root -p 
-echo mysql -u root -p webtech < users.sql
-mysql -u root -p webtech < users.sql
-echo mysql -u root -p webtech < shares.sql 
-mysql -u root -p webtech < shares.sql 
+echo mysql -u root -p < webtech.sql
+mysql -u root -p < webtech.sql
+
--- AlyShmahell-BCompSci-UnivAQ/ComputerScience/WebTechnologies/FinalProject/shares.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE shares(
-`shareholderid` VARCHAR(767) NOT NULL,
-`stocksymbol` VARCHAR(10) NOT NULL,
-`stockcount` INT(30) NOT NULL,
-PRIMARY KEY (`shareholderid`,`stocksymbol`)
-);
