da5914204cdad2fc8e39707900725ce55830403b
AlyShmahell <aly.shmahell@gmail.com>
2017-01-29 17:16:29 +0100

    login and registration complete! I guess :)

--- AlyShmahell-BCompSci-UnivAQ/ComputerScience/WebTechnologies/FinalProject/functions.php
+++ AlyShmahell-BCompSci-UnivAQ/ComputerScience/WebTechnologies/FinalProject/functions.php
@@ -30,8 +30,56 @@ if (!function_exists('login')) {
 		{
 			echo "<h1> Error </h1>";
 			echo "<p> Wrong Credentials! </p>";
-			echo $username;
 		}
+ }
+}
+if (!function_exists('register')) {
+      function register($username,$password) {
+        $username = mysql_real_escape_string($username);
+	$username = preg_replace('/\s+/', '', $username);
+	$password = hash('sha512',mysql_real_escape_string($password));
+	$password = preg_replace('/\s+/', '', $password);
+	$checkusername = mysql_query("SELECT * FROM groups WHERE username='".$username."'");
+	if(mysql_num_rows($checkusername)==1)
+	          {
+		$checkusernameInAdmins = mysql_query("SELECT * FROM admins WHERE username='".$username."'");
+		$checkusernameInUsers = mysql_query("SELECT * FROM users WHERE username='".$username."'");
+		if(mysql_num_rows($checkusernameInAdmins)==1||mysql_num_rows($checkusernameInUsers)==1)
+			{
+				echo "<h1> Error </h1>";
+				echo "<p> Credentials already in use! </p>";
+			}
+		else
+			{
+				$registerquery = mysql_query("INSERT INTO users (username,pass_word) VALUES('".$username."','".$password."')");
+				if($registerquery)
+		              		{
+						echo "<h1>Success 1</h1>";
+						echo "<p>Your account was successfully created. Please <a href=\"index.php\">click here to login</a>.</p>";
+					}
+				else
+		             		 {
+						echo "<h1>Error</h1>";
+						echo "<p>Sorry, your registration failed. Please go back and try again.</p>";
+					}
+			}
+
+	}
+	else
+	          {
+		$registerquery = mysql_query("INSERT INTO groups (username) VALUES('".$username."')");
+		$registerquery = mysql_query("INSERT INTO users (username,pass_word) VALUES('".$username."','".$password."')");
+		if($registerquery)
+		              {
+			echo "<h1>Success 2</h1>";
+			echo "<p>Your account was successfully created. Please <a href=\"index.php\">click here to login</a>.</p>";
+		}
+		else
+		              {
+			echo "<h1>Error</h1>";
+			echo "<p>Sorry, your registration failed. Please go back and try again.</p>";
+		}
+	}
  }
 }
 ?>
--- AlyShmahell-BCompSci-UnivAQ/ComputerScience/WebTechnologies/FinalProject/register.php
+++ AlyShmahell-BCompSci-UnivAQ/ComputerScience/WebTechnologies/FinalProject/register.php
@@ -3,28 +3,7 @@
 <?php
   if(!empty($_POST['username']) && !empty($_POST['password']))
      {
-	$username = mysql_real_escape_string($_POST['username']);
-	$password = hash('sha512',mysql_real_escape_string($_POST['password']));
-	$checkusername = mysql_query("SELECT * FROM users WHERE username='".$username."'");
-	if(mysql_num_rows($checkusername)==1)
-	          {
-		echo "<h1> Error </h1>";
-		echo "<p> Credentials already in use! </p>";
-	}
-	else
-	          {
-		$registerquery = mysql_query("INSERT INTO users (username, password) VALUES('".$username."', '".$password."')");
-		if($registerquery)
-		              {
-			echo "<h1>Success</h1>";
-			echo "<p>Your account was successfully created. Please <a href=\"index.php\">click here to login</a>.</p>";
-		}
-		else
-		              {
-			echo "<h1>Error</h1>";
-			echo "<p>Sorry, your registration failed. Please go back and try again.</p>";
-		}
-	}
+	register($_POST['username'],$_POST['password']);
 }
 else
 {
