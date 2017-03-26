e8f470315058a69367330af810772c9d7eec3bd2
AlyShmahell <aly.shmahell@gmail.com>
2017-03-26 17:58:13 +0200

    added random sequence generator

--- AlyShmahell-BCompSci-UnivAQ/Naive Global Pairwise Alignment/ngpa.py
+++ AlyShmahell-BCompSci-UnivAQ/Naive Global Pairwise Alignment/ngpa.py
@@ -1,5 +1,6 @@
 import sys
-import numpy as np
+import time
+
 scoreMatrix = []
 database = []
 sequences = []
@@ -10,9 +11,11 @@ def score(nucSeq, nucDB):
 
 def ngpa():
 	for i in sequences:
+		startTime = time.time()
 		totalmax = -1000000000
+		perfectSequence = []
 		for j in database:
-			print "first shift"
+			# first shift
 			submax = -1000000000
 			totalmax = max(totalmax,submax)
 			for k in range(len(i)):
@@ -20,15 +23,21 @@ def ngpa():
 				for l,m in zip(range(k,len(i)),range(len(j))):
 					alignmentscore = alignmentscore + score(i[l],j[m])
 				submax = max(submax,alignmentscore)
-			print "second shift"
+			# second shift
+			if submax > totalmax:
+				perfectSequence = j
 			totalmax = max(totalmax,submax)
 			for k in range(len(j)):
 				alignmentscore = 0
 				for l,m in zip(range(k,len(j)),range(len(i))):
 					alignmentscore = alignmentscore + score(j[l],i[m])
 				submax = max(submax,alignmentscore)
+			if submax > totalmax:
+				perfectSequence = j
 			totalmax = max(totalmax,submax)
 		print totalmax
+		print j
+		print time.time()-startTime
 
 
 if __name__=='__main__':
@@ -47,7 +56,6 @@ if __name__=='__main__':
 				temporary = []
 			else:
 				temporary.append(i)
-		print(database)
 
 	with open(sys.argv[2],'r') as sequencesFile:
 		temporary = []
@@ -57,7 +65,5 @@ if __name__=='__main__':
 				temporary = []
 			else:
 				temporary.append(i)
-
-		print(sequences)
 
 	ngpa()
--- AlyShmahell-BCompSci-UnivAQ/Naive Global Pairwise Alignment/randomSequenceGenerator.py
+++ AlyShmahell-BCompSci-UnivAQ/Naive Global Pairwise Alignment/randomSequenceGenerator.py
@@ -1,14 +1,14 @@
 import sys
 import random
 alphabet = ['A','T','G','C']
-for i in range(1,10):
+for i in range(1,6):
 	sequences = ""
 	for j in range(i*10):
 		for k in range(1,10):
 			sequence = ""
-			for l in range(k*100):
+			for l in range(k*10):
 				sequence+=random.choice(alphabet)
 			sequences += sequence
 			sequences += "\n"
-	with open("ngpaDatabase"+str(i),'wr') as database:
+	with open("DNAinput"+str(i),'wr') as database:
 		database.write(sequences)
