4cd81985765f0a18d6ae5dd36956c97ef5b6ad53
Aly Shmahell <aly.shmahell@gmail.com>
2018-04-18 19:15:04 +0200

    updated requirements script

--- AlyShmahell-BCompSci-UnivAQ/AlyShmahell-ThesisProject/neurencoder/requirements.sh
+++ AlyShmahell-BCompSci-UnivAQ/AlyShmahell-ThesisProject/neurencoder/requirements.sh
@@ -19,6 +19,7 @@
 	export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
 	export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/extras/CUPTI/lib64
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
         export CUDA_HOME=/usr/local/cuda-9.0
 	# install numpy, matplot
 	sudo pip3 install --no-cache-dir numpy matplotlib
